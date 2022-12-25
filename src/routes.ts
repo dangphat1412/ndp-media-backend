5facefdf273f18a5e73690d177c200b24a2e13a4
dangphat1412 <phatnguyen1412@gmail.com>
2022-12-25 22:53:32 +0700

    ndp-media

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+// import { Application } from 'express';
+
+// const BASE_AUTH = '/api/v1';
+
+// export default (app: Application) => {
+//   const routes = () => {
+//     app.use(BASE_AUTH, authR)
+//   }
+// }
---
+++ src/setupServer.ts
@@ -0,0 +1,107 @@
+import { Application, json, urlencoded, Response, Request, NextFunction } from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import cookierSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+
+const SERVER_PORT = 5000;
+const log: Logger = config.createLogger('server');
+
+export class NdpMediaServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleWare(this.app);
+    this.standardMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleWare(app: Application): void {
+    app.use(
+      cookierSession({
+        name: 'session',
+        keys: ['test1', 'test2'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
+
+  private routeMiddleware(app: Application): void {}
+
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use()
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
+      this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.error(error);
+    }
+  }
+
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
+    });
+    const pubClient = createClient({ url: process.env.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
+
+  private startHttpServer(httpServer: http.Server): void {
+    log.info(`Server has started with process ${process.pid}`);
+
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Server running on port ${SERVER_PORT}`);
+    });
+  }
+
+  private socketIOConnections(io: Server): void {
+    log.info('socketIOConnections');
+  }
+}
